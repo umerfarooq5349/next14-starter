73bd48c31818c056e7037d3d51d28f7b0da7512e
Umer Farooq <03-135202-015@student.bahria.edu.pk>
2024-03-07 20:53:43 +0500

    images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
 const AboutPage = () => {
-    return <div>About page!</div>;
+    return <div> 
+      
+      
+    <Image src={'/about.png'} width={500} height={500}/>
+    </div>
+    
   };
   
   export default AboutPage;
